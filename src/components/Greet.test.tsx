76ff82898ce9baa8813b7815e7bbcd76b7fefcd7
Prathmesh Wakodikar <46840479+Prathm98@users.noreply.github.com>
2023-01-04 03:26:03 +0530

    Grouping on tests

--- src/components/Greet.test.tsx
+++ src/components/Greet.test.tsx
@@ -1,14 +1,23 @@
 import { render, screen } from "@testing-library/react"
 import Greet from "./Greet"
-
-test("Greet Renders Correctly", () => {
-    render(<Greet />)
-    const expectedText = screen.getByText(/hello/i)
-    expect(expectedText).toBeInTheDocument()
+describe("Greet", () => {
+    test("Renders Correctly", () => {
+        render(<Greet />)
+        const expectedText = screen.getByText(/hello/i)
+        expect(expectedText).toBeInTheDocument()
+    })
 })
 
-test("Greet Renders Correctly with Name", () => {
-    render(<Greet name="User" />)
-    const expectedText = screen.getByText("Hello User")
-    expect(expectedText).toBeInTheDocument()
+describe("Greet with name", ()=>{
+    test("Renders Correctly with User", () => {
+        render(<Greet name="User" />)
+        const expectedText = screen.getByText("Hello User")
+        expect(expectedText).toBeInTheDocument()
+    })
+    
+    test("Renders Correctly with User1", () => {
+        render(<Greet name="User1" />)
+        const expectedText = screen.getByText("Hello User1")
+        expect(expectedText).toBeInTheDocument()
+    })
 })
