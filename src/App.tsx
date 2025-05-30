0a585250be4f145de9e7ac3e80fbd33d3012f20b
Prathmesh Wakodikar <46840479+Prathm98@users.noreply.github.com>
2025-05-31 02:20:03 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" onClick={() => setCount(prev => prev+1)} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -16,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React {count}
         </a>
       </header>
     </div>
